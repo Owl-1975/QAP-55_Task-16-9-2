74ff28e56f3de94298868fa57538fd5f4864ad27
Алексей Будовский <xfowl_75@icloud.com>
2022-01-14 15:50:00 +0300

    Write code to describe a geometric figure. Create a "rectangle" class with the __init__() method. At the output in the console, you need to get the area of this figure. Supplemented with an example of calculating the area of a triangle.

---
+++ calculation of the area.py
@@ -0,0 +1,32 @@
+import math
+
+
+class Rectangle:
+    def __init__(self, w, h):
+        self.width = w
+        self.height = h
+
+    def get_area(self):
+        return self.width * self.height
+
+    def __str__(self):
+        return f'Площадь прямоугольника  {self.get_area()}'
+
+"BONUS, Скучно, два раза подряд, площадь прямоугольника считать!"
+
+class Triangle:
+    def __init__(self, a, b, c):
+        self.side_a = a
+        self.side_b = b
+        self.side_c = c
+        self.sumside = (a + b + c) / 2
+    def get_area(self):
+        return round(math.sqrt(self.sumside * (self.sumside - self.side_a) * (self.sumside - self.side_b) * (self.sumside - self.side_c)), 2)
+    def __str__(self):
+        return f'Площадь треугольника {self.get_area()}'
+
+rectangle = Rectangle(7, 9)
+triangle = Triangle(11, 13, 15)
+
+print(rectangle)
+print(triangle)
